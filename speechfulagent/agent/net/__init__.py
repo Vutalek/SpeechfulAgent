c9fa045981a5f043e31a5dae5edb90067bbf88cf
VutalekSK <mr.sergei02@mail.ru>
2026-02-24 14:58:13 +0300

    changed import of nets

--- speechfulagent/agent/net/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dqn import MLPDQN
-from .a2c import DicreteA2C, ContinuousA2C
-from .ddpg import DiscreteDDPGActor, ContinuousDDPGActor, DDPGCritic
-from .ppo import DiscretePPOActor, ContinuousPPOActor, PPOCritic
