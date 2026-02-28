55aa377a6d4ef05fd382cb80efe01eaab741d35c
VutalekSK <mr.sergei02@mail.ru>
2026-02-28 12:10:06 +0300

    ddpg maybe fixed

--- speechfulagent/train/agent/ddpg.py
+++ speechfulagent/train/agent/ddpg.py
@@ -131,7 +131,6 @@ class DDPGTrainer(BaseTrainer):
         while True:
             n_iter += 1
             exp = self.agent.step()
-            # self.agent.ou_epsilon *= 0.999
             self.replay_buffer.append(exp)
             if exp.done:
                 reward = self.agent.total_reward
